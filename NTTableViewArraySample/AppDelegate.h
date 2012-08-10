837e8522a526e7b2eeda4cfd56122dcb08261c68
Ethan Nagel <eanagel@gmail.com>
2012-08-10 11:10:10 -0700

    Initial Commit

---
+++ NTTableViewArraySample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NTTableViewArraySample
+//
+//  Created by Ethan Nagel on 8/10/12.
+//  Copyright (c) 2012 Nagel Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
